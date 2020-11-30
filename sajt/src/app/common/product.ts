88eb8fdfd5e402c662f9045f1d8f20ef566149cc
wldkstsk <osackij@gmail.com>
2020-11-30 12:44:05 +0300

    See an information about condition of a product and it“s parameters. Frontend. Add Condition field and Description field to product class.

--- sajt/src/app/common/product.ts
+++ sajt/src/app/common/product.ts
@@ -1,15 +1,17 @@
 export class Product {
-    id: string;
-    name: string;
-    firstName: string;
-    phoneNumber: string;
-    description: string;
-    unitPrice: number;
-    imageUrl: string;
-    active: boolean;
-    dateCreated: Date;
-    lastUpdated: Date;
-    city: string;
-    category: string;
-    userId: number;
+  id: string;
+  name: string;
+  firstName: string;
+  phoneNumber: string;
+  description: string;
+  unitPrice: number;
+  imageUrl: string;
+  active: boolean;
+  dateCreated: Date;
+  lastUpdated: Date;
+  city: string;
+  category: string;
+  userId: number;
+  userDescription: string;
+  condition: string;
 }
