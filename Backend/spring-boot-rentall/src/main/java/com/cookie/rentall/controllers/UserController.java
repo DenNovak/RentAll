e19c6ef47f90871b68bd09c0a331a10f3dd94336
wldkstsk <osackij@gmail.com>
2020-12-12 19:15:36 +0300

    Be able to change and save settings in my account. Backend. Add rest api to update email, name, description

---
+++ Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/controllers/UserController.java
@@ -0,0 +1,45 @@
+package com.cookie.rentall.controllers;
+
+import com.cookie.rentall.auth.User;
+import com.cookie.rentall.auth.UserDetailsImpl;
+import com.cookie.rentall.auth.UserRepository;
+import com.cookie.rentall.requests.UserUpdateRequest;
+import com.cookie.rentall.views.UserView;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@CrossOrigin
+@RestController
+public class UserController {
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PutMapping("api/user/{id}")
+    public UserView cancelReservation(@PathVariable("id") Long id, @RequestBody UserUpdateRequest request) {
+        Optional<User> user = userRepository.findById(id);
+        if (!user.isPresent() || !user.get().getId().equals(getUserId())) {
+            return null;
+        }
+        if (request.getFirstName() != null) user.get().setFirstName(request.getFirstName());
+        if (request.getLastName() != null) user.get().setLastName(request.getLastName());
+        if (request.getEmail() != null) user.get().setEmail(request.getEmail());
+        if (request.getDescription() != null) user.get().setDescription(request.getDescription());
+        userRepository.save(user.get());
+        return new UserView(user.get());
+    }
+
+    private Long getUserId() {
+        try {
+            return ((UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
