556889b43f27eb4ad0320f6be36d4690da3e761c
wldkstsk <osackij@gmail.com>
2020-12-20 20:12:15 +0300

    Add media (images) to my advertisement — missing multiple images. Backend. Created storage service implementation

---
+++ Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/services/StorageServiceImpl.java
@@ -0,0 +1,49 @@
+package com.cookie.rentall.services;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+@Service
+public class StorageServiceImpl implements StorageService {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void store(MultipartFile file) {
+        try(FileOutputStream fileOutputStream = new FileOutputStream("images/" + file.getName())) {
+            fileOutputStream.write(file.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Stream<Path> loadAll() {
+        return null;
+    }
+
+    @Override
+    public Path load(String filename) {
+        return null;
+    }
+
+    @Override
+    public Resource loadAsResource(String filename) {
+        return new FileSystemResource(new File("images/" + filename));
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
