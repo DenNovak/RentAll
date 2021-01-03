a9553e159cfa57b99e41790ec9d571fa0a6ae80d
Janiculus <30663986+Janiculus@users.noreply.github.com>
2021-01-03 23:58:27 +0100

    Image table

---
+++ Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/entity/Image.java
@@ -0,0 +1,45 @@
+package com.cookie.rentall.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "image")
+@Data
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "user_id")
+    private long userId;
+
+    @Column(name = "filename")
+    private String filename;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+}
