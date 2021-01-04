c12cea8a094d436fe6272daaa86c0ad5b5f5b93d
wldstsk <osackij@gmail.com>
2021-01-04 04:36:03 +0100

    added link to profile in product and booking vieww

--- sajt/src/app/view-category/category/product-details/product-details.component.ts
+++ sajt/src/app/view-category/category/product-details/product-details.component.ts
@@ -4,8 +4,11 @@ import {ProductService} from 'src/app/services/product.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {AppComponent} from "../../../app.component";
 import {NgbDate, NgbDatepicker, NgbDateStruct} from "@ng-bootstrap/ng-bootstrap";
-import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import {AppComponent} from '../../../app.component';
+import {NgbDate, NgbDatepicker, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import {faCalendarAlt} from '@fortawesome/free-solid-svg-icons';
 import {ProductUnavailableView} from "../../../common/productunavailableview";
+import {ProductUnavailableView} from '../../../common/productunavailableview';
 
 @Component({
   selector: 'app-product-details',
@@ -22,7 +25,7 @@ export class ProductDetailsComponent implements OnInit {
   faCalendar = faCalendarAlt;
   markDisabled: (date: NgbDate) => {};
   refusedDates: ProductUnavailableView[];
-
+  totalCost = 0;
   constructor(private productService: ProductService,
               private route: ActivatedRoute, private router: Router) {
   }
@@ -30,23 +33,16 @@ export class ProductDetailsComponent implements OnInit {
     this.appComponent = this.productService.getAppComponent();
     this.route.paramMap.subscribe(() => {
       this.handleProductDetails();
-      this.productService.listUnavailableDates(this.product.id).subscribe((data) => {
-        this.refusedDates = data;
-        this.disableDays();
-      });
     });
-    this.disableDays()
   }
-
   disableDays() {
-    this.markDisabled = (date: NgbDate) => true;
     this.markDisabled = (date: NgbDate) => {
       for (const interval of this.refusedDates) {
-        if (interval.start === null || interval.end == null) {
+        if (interval.start === null || interval.end === null || interval.start === undefined || interval.end === undefined) {
           continue;
         }
-        const start = new NgbDate(interval.start.getFullYear(), interval.start.getMonth(), interval.start.getDay());
-        const end = new NgbDate(interval.end.getFullYear(), interval.end.getMonth(), interval.end.getDay());
+        const start = new NgbDate(interval.start.getFullYear(), interval.start.getMonth() + 1, interval.start.getDate() - 1);
+        const end = new NgbDate(interval.end.getFullYear(), interval.end.getMonth() + 1, interval.end.getDate() - 1);
         if ((date.after(start) || date.equals(start)) && (date.before(end) || date.equals(end))) {
           return true;
         }
@@ -54,25 +50,74 @@ export class ProductDetailsComponent implements OnInit {
       return false;
     };
   }
-
-
+  refreshCost(nVal: string, num: number) {
+    const dt = new Date(nVal);
+    const newVal = new NgbDate(dt.getFullYear(), dt.getMonth(), dt.getDate());
+    let fr = null;
+    let t = null;
+    if (this.from != null) {
+      fr = new NgbDate(this.from.year, this.from.month - 1, this.from.day - 1);
+    }
+    if (this.to != null) {
+      t = new NgbDate(this.to.year, this.to.month - 1, this.to.day - 1);
+    }
+    if (num === 1) {
+      fr = new NgbDate(newVal.year, newVal.month, newVal.day);
+    } else {
+      t = new NgbDate(newVal.year, newVal.month, newVal.day);
+    }
+    if (t.after(fr) || t.equals(fr)) {
+      const diff = Math.abs(new Date(t.year, t.month, t.day).getTime() - new Date(fr.year, fr.month, fr.day).getTime());
+      const diffDays = Math.ceil(diff / (1000 * 3600 * 24)) + 1;
+      this.totalCost = this.product.unitPrice * diffDays;
+      return;
+    }
+    this.totalCost = 0;
+  }
   handleProductDetails() {
     // get the "id" param string. Convert string to a number using the + symbol
     const theProductId: number = +this.route.snapshot.paramMap.get('id');
     this.productService.getProduct(theProductId).subscribe(
       data => {
         this.product = data;
+        this.productService.listUnavailableDates(this.product.id).subscribe((d) => {
+          this.refusedDates = d.map(rec => {
+            const v = new ProductUnavailableView();
+            v.start = new Date(rec.start);
+            v.end = new Date(rec.end);
+            return v;
+          });
+          this.disableDays();
+        });
       }
     );
     this.productService.getProductStatus(theProductId).subscribe(
-      data => { this.productStatus = data.status; }
+      data => {
+        this.productStatus = data.status;
+      }
     );
     this.productService.getProductConsumer(theProductId).subscribe(
-      data => { this.productConsumer = data; }
+      data => {
+        this.productConsumer = data;
+      }
     );
   }
+  deleteProduct(id: string) {
+    this.productService.deleteProduct(id).subscribe(result => {
+      if (result) {
+        this.router.navigate(['/category']);
+      } else {
+        alert('Failed to delete Product');
+      }
+    });
+  }
   reserveProduct(id: string) {
-    this.productService.reserveProduct(id).subscribe(
+    if (!this.checkDates()) {
+      return;
+    }
+    const st = new Date(this.from.year, this.from.month - 1, this.from.day + 1);
+    const f = new Date(this.to.year, this.to.month - 1, this.to.day + 1);
+    this.productService.reserveProduct(id, st, f).subscribe(
       result => {
         if (result === true) {
           alert('Product reserved');
@@ -82,6 +127,29 @@ export class ProductDetailsComponent implements OnInit {
         }
       }
     );
+  }
+  checkDates(): boolean {
+    if (this.from === null || this.to === null || this.from === undefined || this.to === undefined) {
+      alert('Fill reservation dates');
+      return  false;
+    }
+    if (this.refusedDates === null || this.refusedDates === undefined) {
+      return true;
+    }
+    const fr = new NgbDate(this.from.year, this.from.month - 1, this.from.day - 1);
+    const t = new NgbDate(this.to.year, this.to.month - 1, this.to.day - 1);
+    for (const period of this.refusedDates) {
+      const st = new NgbDate(period.start.getFullYear(), period.start.getMonth() - 1, period.start.getDate() - 1);
+      const f = new NgbDate(period.end.getFullYear(), period.end.getMonth() - 1, period.end.getDate() - 1);
+      if (this.isInInterval(fr, st, f) || this.isInInterval(t, st, f)) {
+        alert('Selected date interval is not free');
+        return false;
+      }
+    }
+    return true;
+  }
+  isInInterval(d: NgbDate, s: NgbDate, f: NgbDate) {
+    return (d.after(s) || d.equals(s)) && (d.before(f) || d.equals(f));
   }
   cancelProductReservation(id: string) {
     this.productService.cancelReservation(id).subscribe(
