8f0c16d7970de9a162bb2203ded7b199b734df59
wldkstsk <osackij@gmail.com>
2021-01-16 18:37:37 +0300

    Be abble to rate users and leave opinions of users. Backend. Add OpinionCreateRequest.

---
+++ Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/requests/OpinionCreateRequest.java
@@ -0,0 +1,31 @@
+package com.cookie.rentall.requests;
+
+public class OpinionCreateRequest {
+    private Long userId;
+    private String content;
+    private Integer rating;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+}
