b1e1bf15d7db3b3c91a06a3fa482f5bd92f1aca7
wldkstsk <osackij@gmail.com>
2021-01-16 18:38:53 +0300

    Be abble to rate users and leave opinions of users. Backend. Add OpinionView.

---
+++ Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/views/OpinionView.java
@@ -0,0 +1,31 @@
+package com.cookie.rentall.views;
+
+public class OpinionView {
+    private Long authorId;
+    private String content;
+    private Integer rating;
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+}
