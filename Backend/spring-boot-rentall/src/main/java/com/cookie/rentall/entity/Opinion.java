2a1e9a28f0cebb8dd28a28fe24708f1cb2779ac6
wldkstsk <osackij@gmail.com>
2021-01-16 18:33:23 +0300

    Be abble to rate users and leave opinions of users. Backend. Add Opinion table.

---
+++ Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/entity/Opinion.java
@@ -0,0 +1,70 @@
+package com.cookie.rentall.entity;
+
+import com.cookie.rentall.auth.User;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "opinion")
+@Data
+public class Opinion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "rating")
+    private Integer rating;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+}
