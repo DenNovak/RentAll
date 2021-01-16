93abebf29e69e2d90e7cfd6fda74e4da0d3269d6
wldkstsk <osackij@gmail.com>
2021-01-16 19:35:11 +0300

    Be abble to rate users and leave opinions of users. Frontend. Add Extend UserView.

--- sajt/src/app/common/user.ts
+++ sajt/src/app/common/user.ts
@@ -1,6 +1,10 @@
+import {Opinion} from "./opinion";
+
 export class User {
   firstName: string;
   lastName: string;
   description: string;
   email: string;
+  opinions: Opinion[];
+  rating: number;
 }
