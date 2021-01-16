d0aabaf58917dd1dc195ef0bb3237cbc71b170e5
wldkstsk <osackij@gmail.com>
2021-01-16 22:05:33 +0300

    Be abble to rate users and leave opinions of users. Frontend. Add opinion create controls to user page.

--- sajt/src/app/user-view/user-view.component.ts
+++ sajt/src/app/user-view/user-view.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {AppComponent} from '../app.component';
 import {User} from '../common/user';
 import {ProductService} from '../services/product.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AlertService} from "../_alert";
 
 @Component({
   selector: 'app-user-view',
@@ -15,8 +16,12 @@ export class UserViewComponent implements OnInit {
   userId: number;
   authorMap: Map<number, User> = new Map<number, User>();
   ratings: Array<number>;
+  yourOpinion: string;
+  yourRating: number = 0;
+  mouseUprating: number = 0;
 
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute, private alertService: AlertService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.appComponent = this.productService.getAppComponent();
@@ -27,7 +32,7 @@ export class UserViewComponent implements OnInit {
   loadUser() {
     this.productService.getUser(this.userId).subscribe(data => {
       this.user = data;
-      this.ratings = Array(this.user.rating).fill(0).map((x, i) => i);
+      this.ratings = Array(Math.round(this.user.rating)).fill(0).map((x, i) => i);
       this.user.opinions.forEach(o => {
         this.productService.getUser(o.authorId).subscribe(author => {
           this.authorMap.set(o.authorId, author);
@@ -38,5 +43,42 @@ export class UserViewComponent implements OnInit {
 
   counter(i: number) {
     return new Array(i);
+  }
+
+  sendOpinion() {
+    this.productService.createOpinion(this.userId, this.yourOpinion, this.yourRating).subscribe(data => {
+      this.alertService.success(data);
+      window.location.reload();
+    });
+  }
+
+  resetRatingMouse(index) {
+    this.mouseUprating = 0;
+  }
+
+  setRatingMouse(index) {
+    this.mouseUprating = index + 1;
+  }
+
+  getMouseRating() {
+    if (this.mouseUprating >= this.yourRating) {
+      return this.mouseUprating;
+    }
+    return this.yourRating;
+  }
+
+  getStarImage(index) {
+    if (index < this.getMouseRating()) {
+      return 'assets/images/star.png';
+    }
+    return 'assets/images/star_grey.png';
+  }
+
+  fixRating(index) {
+    this.yourRating = index + 1;
+  }
+
+  hasOpinion() {
+    return this.appComponent.userId === this.userId || this.user.opinions.find(o => o.authorId === this.appComponent.userId) !== undefined;
   }
 }
