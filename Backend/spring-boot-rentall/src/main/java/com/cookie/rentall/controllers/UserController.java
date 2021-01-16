abe70396354e69bb1d8dbe8eae2a6266b78b924c
wldkstsk <osackij@gmail.com>
2021-01-16 19:00:00 +0300

    Be abble to rate users and leave opinions of users. Backend. Add API to create opinion.

--- Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/controllers/UserController.java
+++ Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/controllers/UserController.java
@@ -3,6 +3,9 @@ package com.cookie.rentall.controllers;
 import com.cookie.rentall.auth.User;
 import com.cookie.rentall.auth.UserDetailsImpl;
 import com.cookie.rentall.auth.UserRepository;
+import com.cookie.rentall.entity.Opinion;
+import com.cookie.rentall.repositores.OpinionRepository;
+import com.cookie.rentall.requests.OpinionCreateRequest;
 import com.cookie.rentall.requests.UserUpdateRequest;
 import com.cookie.rentall.views.UserView;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,9 +18,11 @@ import java.util.Optional;
 @RestController
 public class UserController {
     private final UserRepository userRepository;
+    private final OpinionRepository opinionRepository;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, OpinionRepository opinionRepository) {
         this.userRepository = userRepository;
+        this.opinionRepository = opinionRepository;
     }
 
     @GetMapping("api/user/{id}")
@@ -39,6 +44,22 @@ public class UserController {
         if (request.getDescription() != null) user.get().setDescription(request.getDescription());
         userRepository.save(user.get());
         return new UserView(user.get());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("api/user/opinion")
+    public Boolean createOpinion(@RequestBody OpinionCreateRequest request) {
+        Optional<User> user = userRepository.findById(request.getUserId());
+        if (!user.isPresent() || user.get().getId().equals(getUserId())) {
+            return false;
+        }
+        Opinion opinion = new Opinion();
+        opinion.setAuthor(userRepository.getOne(getUserId()));
+        opinion.setUser(user.get());
+        opinion.setContent(request.getContent());
+        opinion.setRating(request.getRating());
+        opinionRepository.save(opinion);
+        return true;
     }
 
     private Long getUserId() {
