ead8996f4a2d3556cbef2102a5ffe0d02d092cb5
wldkstsk <osackij@gmail.com>
2021-01-16 18:35:34 +0300

    Be abble to rate users and leave opinions of users. Backend. Add Opinion Repository.

---
+++ Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/repositores/OpinionRepository.java
@@ -0,0 +1,7 @@
+package com.cookie.rentall.repositores;
+
+import com.cookie.rentall.entity.Opinion;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OpinionRepository  extends CrudRepository<Opinion, Long> {
+}
