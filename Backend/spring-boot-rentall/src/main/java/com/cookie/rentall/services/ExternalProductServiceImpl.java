b459ff77dfe19e2db0169b76589021c780fc8048
wldkstsk <osackij@gmail.com>
2021-01-23 19:33:02 +0300

    Fixed integration

--- Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/services/ExternalProductServiceImpl.java
+++ Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/services/ExternalProductServiceImpl.java
@@ -23,21 +23,23 @@ import java.util.regex.Pattern;
 
 @Service
 public class ExternalProductServiceImpl implements ExternalProductService {
-    private static final String PRODUCT_REGEX = "\"@type\": \"Product\"(([\\s\\S](?!\"@type\": \"Product\"))*)\"itemReviewed\":";
+    private static final String PRODUCT_REGEX = "<div class=\"c-grid_col is-grid-col-1\"><div class=\"c-offerBox is-wide  is-available\"(([\\s\\S](?!<div class=\"c-grid_col is-grid-col-1\"><div class=\"c-offerBox is-wide  is-available\"))*)Darmowy transport";
     private static final String PRODUCT_URL = "https://www.mediaexpert.pl/dom-i-ogrod/kosiarki-i-podkaszarki/kosiarki-spalinowe";
     private static final String POJEMNOSC_SILNIKA_START = "Pojemność silnika \\[cm3\\]";
     private static final String SILNIK_MARKA = "Silnik marka";
     private static final String SZEROKODC_KOSZENIA = "Szerokość koszenia \\[mm\\]";
     private static final String REGULACJA_WYSOKOSCI_KOSZENIA = "Regulacja wysokości koszenia";
     private static final String POJEMNOSC_KOSZA = "Pojemność kosza \\[l\\]";
-    private static final String PRODUCT_NAME = "\"og:title\" content=\"";
+    private static final String PRODUCT_NAME = "<meta data-analytics-item='\\{\"id\":\"[0-9]+\",\"name\":\"";
+    private static final String PRODUCT_ID = "<meta data-analytics-item='\\{\"id\":\"";
+    private static final String PRODUCT_CATEGORY = "<meta data-analytics-item='\\{(([\\s\\S](?!category))*)\"category\":\"";
     private static final String PRODUCT_PHOTO = "data-zone=\"OFFERBOX_PHOTO\" data-offer-id=(([\\s\\S](?!/div>))*)>";
-    private static final String PRODUCT_PHOTO_INTERNAL = "src=\"(([\\s\\S](?!\"></div>))*)\"";
+    private static final String PRODUCT_PHOTO_INTERNAL = "src=\"(([\\s\\S](?!\" ))*)>";
 
     @Override
     public List<ExternalProductView> getExternalProductList() {
         List<ExternalProductView> resultList = new ArrayList<>();
-        for(int i = 0; i < 3; i++) { //todo учитывать количество страниц
+        for (int i = 0; i < 3; i++) { //todo учитывать количество страниц
             final String uri = PRODUCT_URL + "?page=" + i;
             RestTemplate restTemplate = createRestTemplate();
             HttpEntity<String> entity = createStringHttpEntity();
@@ -50,15 +52,35 @@ public class ExternalProductServiceImpl implements ExternalProductService {
                 sb.append(matcher.group());
                 sb.append("\"\"} },");
                 String pr = sb.toString().replace("@type", "type");
-                try {
-                    resultList.add(getExternalProduct(mapper.readValue(pr, ExternalProductListItem.class)));
-                    sb = new StringBuilder("{");
-                } catch (JsonProcessingException e) {
+                //try {
+                resultList.add(getExternalProduct(pr));
+                sb = new StringBuilder("{");
+                /*} catch (JsonProcessingException e) {
                     e.printStackTrace();
-                }
+                }*/
             }
         }
         return resultList;
+    }
+
+    private ExternalProductView getExternalProduct(String rawProduct) {
+        ExternalProductView result = new ExternalProductView();
+        //result.setProductID(item.getProductID());
+        RestTemplate restTemplate = createRestTemplate();
+        HttpEntity<String> entity = createStringHttpEntity();
+        result.setPojemnoscSilnika(parseFieldValue(rawProduct, POJEMNOSC_SILNIKA_START, "</td>", false));
+        result.setMarkaSilnika(parseFieldValue(rawProduct, SILNIK_MARKA, "</td>", false));
+        result.setSzerokoscKoszenia(parseFieldValue(rawProduct, SZEROKODC_KOSZENIA, "</td>", false));
+        result.setRegulacjaWysokosciKoszenia(parseFieldValue(rawProduct, REGULACJA_WYSOKOSCI_KOSZENIA, "</td>", false));
+        result.setPojemnoscKosza(parseFieldValue(rawProduct, POJEMNOSC_KOSZA, "</td>", false));
+        if (result.getPojemnoscKosza() == null) result.setPojemnoscKosza("-");
+        result.setName(parseFieldValue(rawProduct, PRODUCT_NAME, ",\"", true, "list"));
+        result.setProductID(parseFieldValue(rawProduct, PRODUCT_ID, ",\"", true, "name"));
+        result.setCategory(parseFieldValue(rawProduct, PRODUCT_CATEGORY, ",\"", true, "variant"));
+        String photo = parseFieldValue(rawProduct, PRODUCT_PHOTO, PRODUCT_PHOTO_INTERNAL);
+        if (photo != null && photo.length() > 5)
+            result.setPhotoLink(photo.substring(5, photo.length() - 2));
+        return result;
     }
 
     private ExternalProductView getExternalProduct(ExternalProductListItem item) {
@@ -83,7 +105,11 @@ public class ExternalProductServiceImpl implements ExternalProductService {
     }
 
     private String parseFieldValue(String rawProduct, String start, String end, boolean isProductName) {
-        Pattern pattern = Pattern.compile(getFieldRegex(start, end, isProductName));
+       return  parseFieldValue(rawProduct, start, end, isProductName, "");
+    }
+
+    private String parseFieldValue(String rawProduct, String start, String end, boolean isProductName, String stop) {
+        Pattern pattern = Pattern.compile(isProductName? getFieldRegex(start, end, stop) : getFieldRegex(start, end, isProductName));
         Matcher matcher = pattern.matcher(rawProduct);
         if (matcher.find()) {
             return getFieldValue(start, matcher.group(), end, isProductName);
@@ -108,8 +134,15 @@ public class ExternalProductServiceImpl implements ExternalProductService {
         return start + (isProductName ? "" : "</em></div></td><td class=\"is-value\">") + "(([\\s\\S](?!/tr))*)" + end;
     }
 
+    private String getFieldRegex(String start, String end, String stop) {
+        return start + "(([\\s\\S](?!" + stop + "))*)" + end;
+    }
+
     private String getFieldValue(String start, String fieldString, String end, boolean isProductName) {
-        int startIndex = (start + (isProductName ? "" : "</em></div></td><td class=\"is-value\">")).length();
+        Pattern pattern = Pattern.compile(isProductName ? start : start + "</em></div></td><td class=\"is-value\">");
+        Matcher matcher = pattern.matcher(fieldString);
+        int startIndex = matcher.find() ? matcher.group().length() : 0;
+        //int startIndex = (start + (isProductName ? "" : "</em></div></td><td class=\"is-value\">")).length();
         int endIndex = fieldString.length() - (end.length() + 1);
         return fieldString.substring(startIndex, endIndex).trim();
     }
